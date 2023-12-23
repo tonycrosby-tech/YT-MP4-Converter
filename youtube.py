d2ebe5e71d9e1815ca4ae6c0f25ba76069307e42
Tony Crosby <tonycrosby96@gmail.com>
2023-12-23 10:15:28 -0600

    added the main functionality

--- youtube.py
+++ youtube.py
@@ -21,4 +21,16 @@ def open_save_path():
     print(f"Selected folder: {save_folder}")
     
     return save_folder
- 
+
+if __name__ == "__main__":
+  root = tk.Tk()
+  root.withdraw()
+  
+  yt_url = input("Please enter a YT url: ")
+  conv_dir = open_save_path()
+  
+  if conv_dir:
+    print("Downloading mp3... Please wait.")
+    yt_mp3(yt_url, conv_dir)
+  else:
+    print("Invalid save location.")
