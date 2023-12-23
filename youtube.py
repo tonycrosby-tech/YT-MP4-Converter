932ca930da8ecd5222a80b59a15ed478f70243c0
Tony Crosby <tonycrosby96@gmail.com>
2023-12-23 10:05:49 -0600

    adding the yt filter to download only mp3.

--- youtube.py
+++ youtube.py
@@ -3,3 +3,14 @@ from pytube import YouTube
 import tkinter as tk
 from tkinter import filedialog
 
+def yt_mp3(url, save_path):
+  """Filter through yt video and select only audio file"""
+  try:
+    yt = YouTube(url)
+    video = yt.streams.filter(only_audio=True).first()
+    video.download(output_path=save_path)
+    print("mp3 downloaded successfully!")
+    
+  except Exception as error:
+    print(error)
+    
